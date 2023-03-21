2b863b6231c2fd722d6c710334f97ed6e1daa713
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2023-03-21 00:08:04 -0500

    fix id=9999

--- api/authors/create.php
+++ api/authors/create.php
@@ -12,6 +12,7 @@ $author = new Author($db);
 
 $data = json_decode(file_get_contents("php://input"));
 
+$author->id = $data->id;
 $author->author = $data->author;
 
 if ($author->create()) {
--- models/Authors.php
+++ models/Authors.php
@@ -70,6 +70,11 @@ class Author
 
         // Execute query
         if ($stmt->execute()) {
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            // set properties
+            $this->id = $row['id'] ?? null;
+            $this->author = $row['category'] ?? null;
             return true;
         }
 
