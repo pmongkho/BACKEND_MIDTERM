164d428657651a2b1b914b8f80a998857e8b53dc
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2023-03-21 17:05:51 -0500

    fix Post requests

--- api/categories/create.php
+++ api/categories/create.php
@@ -1,4 +1,4 @@
-<?php
+<?php error_reporting(0);
 
 include_once '../../config/Database.php';
 include_once '../../models/Categories.php';
@@ -12,7 +12,12 @@ $category = new Category($db);
 
 $data = json_decode(file_get_contents("php://input"));
 
-$category->category = $data->category;
+if ($data->category) {
+    $category->category = $data->category;
+} else {
+    print json_encode(array('message' => 'category_id Not Found'));
+    die();
+}
 
 if ($category->create()) {
     $categoryItem = array(
