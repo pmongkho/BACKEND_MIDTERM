45d6d99e8d55f071019bd41f821eeeaff4599206
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2023-03-21 20:24:48 -0500

    put requests quotes left

--- api/quotes/delete.php
+++ api/quotes/delete.php
@@ -19,14 +19,16 @@ $quote->id = $data->id;
 // Delete quote
 if ($quote->delete()) {
     $row = $quote->read_single()->rowCount();
-        if($row == 0){
+    if ($row == 0) {
         print json_encode(
             array('message' => 'Quote Not Found')
         );
-        }
-    print json_encode(
-        array('id' => $quote->id)
-    );
+    } else {
+        print json_encode(
+            array('id' => $quote->id)
+        );
+    }
+
 } else {
     print json_encode(
         array('message' => 'Quote not deleted')
